cb3d62ed629b92f16f1d1780ddf71d701c009272
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-05 10:55:06 -0600

    Adding load event to wait for bundle file to completely finish loading before rendering

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
-import domready from 'domready'
 import Root from './components/Root'
 import configureStore from './store/configureStore'
 import * as api from './api/database'
@@ -10,7 +9,7 @@ import './styles/index.scss'
 const store = configureStore(api.getStorage())
 
 // render application to page
-domready(() => {
+window.addEventListener('load', () => {
 	render(
 		<div>
 			<Root store={store} />
