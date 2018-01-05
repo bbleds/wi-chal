373af94a1c684fc41ff1bab625fe0590912d7f6d
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-05 06:41:27 -0600

    Began clean up process on database.js

--- client/src/api/database.js
+++ client/src/api/database.js
@@ -3,61 +3,48 @@ import initialState from '../store/initialState'
 
 const applicationStateKey = 'todo-app-state'
 
-let saveStorage = (data) => {
-	window.localStorage.setItem(applicationStateKey, JSON.stringify(newState))
-}
+let saveStorage = data =>  window.localStorage.setItem(applicationStateKey, JSON.stringify(data))
 
-
-// retrive entire application state from data storage
+// handles retrieving application state from data storage
 export let getStorage = () => {
 	let existingState = JSON.parse(window.localStorage.getItem(applicationStateKey))
 	return existingState || initialState
 }
 
 // handles persiting a todo into data storage
-export let addTodo = (todo) => {
-	// append new todoId
+export let addTodo = todo => {
+
 	todo.todoId = uuid()
 
-	let state = getStorage()
-	let todos = state.todos.concat(todo)
-	let newState = {
-		...state,
-		todos
-	}
-
-	window.localStorage.setItem(applicationStateKey, JSON.stringify(newState))
+	let newState = getStorage()
+	let todos = newState.todos.concat(todo)
+	newState.todos = todos
+	saveStorage(newState)
 
 	return todo
 }
 
-// toggles a todo's "completed" property and saves it into data storage
-export let toggleTodo = (todoId) => {
-	let todos = getStorage().todos
+// handles toggling a todo's "completed" property and saves it into data storage
+export let toggleTodo = todoId => {
+	let newState = getStorage()
+	let todos = newState.todos
 
-	todos = todos.map((item)=>{
-
-		 if(item.todoId == todoId){
-			 item.completed = !item.completed
-		 }
-
+	todos = todos.map( item => {
+		 if (item.todoId == todoId) item.completed = !item.completed
 		 return item
 	})
 
-	let state = getStorage()
-	state.todos = todos
-	let newState = {
-		...state,
-		todos
-	}
-
-	window.localStorage.setItem(applicationStateKey, JSON.stringify(newState))
+	newState.todos = todos
+	saveStorage(newState)
 
 	return todoId
 }
 
-export let setTodosVisibilty = (value) => {
+// handles setting the todos visibility by a user-selected filter
+export let setTodosVisibilty = value => {
 	let newState = getStorage()
 	newState.todosVisibility = value
-	window.localStorage.setItem(applicationStateKey, JSON.stringify(newState))
+	saveStorage(newState)
+
+	return true
 }
