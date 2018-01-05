0634cef789354c53412bcab7d68cdff0a2f08fff
Ben Bledsoe <bledsoedesigns@gmail.com>
2018-01-05 09:07:11 -0600

    Refactoring main todo component for clarity

--- client/src/components/TodoPage/TodoContainer.js
+++ client/src/components/TodoPage/TodoContainer.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react'
+import {render} from 'react-dom'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import * as actions from '../../actions/todoActions'
 
-// include our components
-import {render} from 'react-dom'
+// include custom components
 import TodoList from './TodoList'
 import TodoForm from './TodoForm'
 import TodoFooter from './TodoFooter'
 
 class TodoContainer extends Component{
+	handleFormSubmit(value){
+		this.props.actions.cleanErrors()
+		this.props.actions.addTodo(value)
+	}
+
+	handleFormSubmitError(msg){
+		this.props.actions.generateError(msg)
+	}
+
+	handleToggleTodo(todoId){
+		this.props.actions.toggleTodo(todoId)
+	}
+
+	handleTodoFilter(value){
+		this.props.actions.setTodosVisibilty(value)
+	}
+
 	render(){
-		let { todos, todosVisibility, errors, state } = this.props
+		let { todos, todosVisibility, errors } = this.props
 
 		return(
 			<div>
@@ -28,32 +45,17 @@ class TodoContainer extends Component{
 						<div id='todo-app-output' className="col-md-offset-3 col-md-6">
 							<h1>To-do App</h1>
 							<TodoForm
-								submitHandler={
-									(value)=>{
-										this.props.actions.cleanErrors()
-										this.props.actions.addTodo(value)
-									}
-								}
-								errorHandler={
-									(msg) => {
-										this.props.actions.generateError(msg)
-									}
-								}
+								submitHandler={ value => this.handleFormSubmit(value) }
+								errorHandler={ msg => this.handleFormSubmitError(msg) }
 							/>
 							<p>Currently viewing - {todosVisibility} </p>
 							<TodoList
 								todos={todos}
-								onChangeHandler={
-									(todoId) => {
-										this.props.actions.toggleTodo(todoId)
-									}
-								}
-								todosVisibility={todosVisibility}
+								onChangeHandler={ todoId => this.handleToggleTodo(todoId) }
+								todosVisibility={ todosVisibility }
 							/>
 							<TodoFooter
-								onClickHandler={(value)=>{
-									this.props.actions.setTodosVisibilty(value)
-								}}
+								onClickHandler={ value => this.handleTodoFilter(value) }
 							/>
 						</div>
 					</div>
@@ -63,6 +65,7 @@ class TodoContainer extends Component{
 	}
 }
 
+// allows us to specify what is passed in as props from redux application state to this react component
 function mapStateToProps(state) {
   return {
     todos: state.todos,
@@ -72,12 +75,14 @@ function mapStateToProps(state) {
   };
 }
 
+// this allows us to create custom actions and inject additional functionality before dispatching final redux actions
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actions, dispatch)
   };
 }
 
+// this connects redux application state with this react component
 export default connect(
   mapStateToProps,
   mapDispatchToProps
